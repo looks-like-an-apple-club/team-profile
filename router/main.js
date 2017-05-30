5155be7adc819bdd62c65d7e8d2e91ce945c2925
inho kwon <ihkwon@rainbow-wireless.com>
2017-05-30 19:07:05 +0900

    schema, api sample coding

--- router/main.js
+++ router/main.js
@@ -1,5 +1,7 @@
 module.exports = function(app)
 {
+    var User = require('../models/user');
+
      app.get('/',function(req,res){
         res.render('index.html')
      });
@@ -14,4 +16,29 @@ module.exports = function(app)
     });
 
     //jinkeonsu add end -----
-}
+
+// 여기서부터 샘플입니다. 테스트 용도로만 사용하세요.
+    app.get('/createuser',function(req,res){
+      var newUser = User({
+        name: 'Peter Quill',
+        username: 'starlord55',
+        password: 'password'
+      });
+
+      // save the user
+      newUser.save(function(err) {
+        if (err) {
+            return res.status(500).send(err);
+        }
+
+        console.log('User created!');
+      });
+    });
+
+    app.get('/users',function(req,res){
+      User.find({}, function (err, docs) {
+        return res.status(200).send(docs);
+      });
+    });
+
+};
