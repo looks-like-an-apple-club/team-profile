80de1a78d81198eda4a2d3d9fab588dd62a2c3f3
JinKeonsu <keonsu.jin@gmail.com>
2017-05-30 15:12:20 +0900

    게시판용 파일 추가, main.js에 라우팅 추가

---
+++ public/assets/js/boardApi.js
@@ -0,0 +1,30 @@
+// 게시판 페이지에서 필요한 자바스크립트 함수들 모아 놓음
+// Jin keonsu
+//
+
+var boardApi = {
+  var mongoose = require('mongoose');
+  mongoose.connect('mongodb://ds155411.mlab.com:55411');
+
+  var db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'connection error:'));
+  db.once('open', function() {
+  // we're connected!
+    console.log("db connected");
+  });
+
+  var testSchema = mongoose.Schema({
+    count: {type:Number, default: 0},
+    data: [{type:Number, default: 0}, {content: String}]
+  });
+  var testModel = mongoose.model('jksTest', testSchema);
+};
+
+boardApi.testFunction = function(){
+    alert("this.testModel.data");
+}
+
+boardApi.getBoardList = function() {
+    var jsonData = {};
+    return jsonData;
+}
--- router/main.js
+++ router/main.js
@@ -6,4 +6,12 @@ module.exports = function(app)
     //  app.get('/about',function(req,res){
     //     res.render('about.html');
     // });
+
+    //jinkeonsu add begin ----
+    //게시판 페이지
+    app.get('/board',function(req,res){
+      res.render('boardList.html');
+    });
+
+    //jinkeonsu add end -----
 }
