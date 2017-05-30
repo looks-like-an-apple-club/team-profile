eccf8e986e59d20d529740af210b20a96dbcb07c
inho kwon <ihkwon@rainbow-wireless.com>
2017-05-30 17:26:43 +0900

    mongoose, dotenv 모듈 추가 및 db connection 처

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 var express = require('express');
 var app = express();
 var router = require('./router/main')(app);
+var mongoose = require('mongoose');
+require('dotenv').config();
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
-
+// mongoose.connect('mongodb://developer:appleclub@ds155411.mlab.com:55411/lookslikeapple');
+var options = {
+  user: process.env.DB_USER,
+  pass: process.env.DB_PASS
+};
+mongoose.connect('mongodb://ds155411.mlab.com:55411/lookslikeapple', options, function(err) {
+      console.log('mongodb connected');
+      if (err) {
+        console.error('mongodb connection error', err);
+      }
+    });
 var server = app.listen(3000, function(){
     console.log("Express server has started on port 3000")
 });
