f89560d496211622fddd0534f051753983385171
JinKeonsu <keonsu.jin@gmail.com>
2017-05-31 10:25:44 +0900

    진건수 본인 이미지 추가 및 관련 코드 수정
    face_jin.jpg 추가
    boardApi.js 수정
    index.html 본인 소개 부분 수정

--- public/assets/js/boardApi.js
+++ public/assets/js/boardApi.js
@@ -1,24 +1,6 @@
 // 게시판 페이지에서 필요한 자바스크립트 함수들 모아 놓음
 // Jin keonsu
-//
 
-// var boardApi = {
-  // var mongoose = require('mongoose');
-  // mongoose.connect('mongodb://ds155411.mlab.com:55411');
-  //
-  // var db = mongoose.connection;
-  // db.on('error', console.error.bind(console, 'connection error:'));
-  // db.once('open', function() {
-  // // we're connected!
-  //   console.log("db connected");
-  // });
-  //
-  // var testSchema = mongoose.Schema({
-  //   count: {type:Number, default: 0},
-  //   data: [{type:Number, default: 0}, {content: String}]
-  // });
-  // var testModel = mongoose.model('jksTest', testSchema);
-// };
 
 testFunction = function(){
     alert("this.testModel.data");
