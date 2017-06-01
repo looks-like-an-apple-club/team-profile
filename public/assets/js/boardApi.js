0ca51e3d6dfff8ba223c7dc7eee91d2ea3514ff5
JinKeonsu <keonsu.jin@gmail.com>
2017-06-01 18:54:30 +0900

    board 관련 파일 수정
    아까부터..
    몽고디비 연결이 안되는 문제..
    이것저것 수정 테스트 진행중이나 일단 백업차원에서 커밋...

--- public/assets/js/boardApi.js
+++ public/assets/js/boardApi.js
@@ -1,11 +1,6 @@
 // 게시판 페이지에서 필요한 자바스크립트 함수들 모아 놓음
 // Jin keonsu
 
-
-testFunction = function(){
-    alert("this.testModel.data");
-};
-
 getBoardList = function() {
     var jsonData = {};
     $.ajax({
@@ -25,9 +20,21 @@ getCommentList = function() {
         url:'./boards/vcomment',
         type:'get',
         success:function(data){
+          console.log('getCommentList success');
           jsonData = data;
-          console.log(data);
         }
     })
     return jsonData;
+};
+
+saveOneComment = function(writer, comment) {
+    var jsonData = {'writer':writer, 'comment':comment};
+    $.ajax({
+        url:'./boards/saveComment',
+        type:'get',
+        data: jsonData,
+        success:function(data){
+          console.log("save one comment success");
+        }
+    })
 };
--- router/board.js
+++ router/board.js
@@ -8,25 +8,29 @@ var router = express.Router();
 // path : /boards/
 // 변경 추천 : '/boards:id' - 하나의 요소만 GET 할 때는 url parameter를 쓰는 형태로 사용합니다.
 router.get('/', function(req, res, next) {
-  VisitorBook.find({}, function (err, docs) {
-      if(err) {
-          console.log('get visitor comment list failed')
-      };
-      res.render('boardList.html', {contents: docs});
-  });
-    // res.render('boardList.html');
+  // VisitorBook.find({}, function (err, docs) {
+  //     if(err) {
+  //         console.log('get visitor comment list failed');
+  //     }
+  //     console.log(docs);
+  //     res.render('boardList.html', {contents: docs});
+  // });
+    res.render('boardList.html');
 });
 
+// path : '/boards/vcomment'
+// 방명록 데이터를 읽어옴
 router.get('/vcomment', function(req, res, next) {
     VisitorBook.find({}, function (err, docs) {
         if(err) {
-            console.log('get visitor comment list failed')
-        };
-        return res.send(docs);
+            console.log('get /vcomment failed');
+        }
+        return res.status(200).send({contents: docs});
     });
 });
 
 // path : '/boards/saveComment'
+// 방명록 데이터를 저장함
 router.post('/saveComment', function(req, res){
     // var curDateTime = new Date();
     // var strDateTime = curDateTime.toLocaleDateString() + " " + curDateTime.toLocaleTimeString();
